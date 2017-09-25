e5aaf81f12d4b0ea050708410a68cfa62ea909af
alphonsejacob <alphonsejacob@mca.ajce.in>
2017-09-26 00:24:21 +0530

    updated home page

---
+++ home.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<!--
+Template Name: Lolwork
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>SewOll</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!--<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> -->
+    <!-- ################################################################################################ -->
+    <!--<div class="fl_left">
+      <ul class="nospace inline pushright">
+        <li><i class="fa fa-sign-in"></i> <a href="#">Login</a></li>
+        <li><i class="fa fa-user"></i> <a href="#">Register</a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <form class="clear" method="post" action="#">
+        <fieldset>
+          <legend>Search:</legend>
+          <input type="search" value="" placeholder="Search Here&hellip;">
+          <button class="fa fa-search" type="submit" title="Search"><em>Search</em></button>
+        </fieldset>
+      </form>
+    </div>-->
+    <!-- ################################################################################################ -->
+ <!--</div>
+</div>-->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">SewOll</a></h1>
+      <p>join or fasten by making stitches</p>
+    </div>
+    <!-- ################################################################################################ -->
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a class="drop" href="#">Pages</a>
+          <ul>
+            <li><a href="pages/gallery.php">Gallery</a></li>
+            <li><a href="pages/full-width.html">Full Width</a></li>
+            <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+            <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+            <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">Dropdown</a>
+          <ul>
+            <li><a href="#">Level 2</a></li>
+            <li><a class="drop" href="#">Level 2 + Drop</a>
+              <ul>
+                <li><a href="#">Level 3</a></li>
+                <li><a href="#">Level 3</a></li>
+                <li><a href="#">Level 3</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Level 2</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Link Text</a></li>
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/01.jpg');">
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+      <h2 class="heading">START YOUR SEWING JOURNEY</h2>
+      <p>The School of Sew classroom is part of a four storey former Victorian mill building of great character, nestled in the picturesque village of Farsley, Leeds.
+
+You’ll enjoy learning and creating in such an enchanting and inspirational environment.
+
+</p>
+      <footer><a class="btn" href="#">CONTACT US</a></footer>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="center btmspace-50">
+      <h2 class="heading">Specialization On</h2>
+      
+    </div>
+    <ul class="nospace group btmspace-50">
+      <li class="one_third first">
+        <article class="element">
+          <figure><img src="images/demo/320x217.jpg" alt="">
+            <figcaption><a class="btn small" href="#">More</a></figcaption>
+          </figure>
+          <div class="excerpt">
+            <time datetime="2045-06-06T08:15+00:00" data-title="June"><strong>Jun</strong> <em>06</em></time>
+            <h6 class="heading"><a href="#">Cutting</a></h6>
+            <p>Teach you to how to cut cloth form basic steps...&hellip;</p>
+          </div>
+        </article>
+      </li>
+      <li class="one_third">
+        <article class="element">
+          <figure><img src="images/demo/320x218.jpg" alt="">
+            <figcaption><a class="btn small" href="#">More</a></figcaption>
+          </figure>
+          <div class="excerpt">
+            <time datetime="2045-05-05T08:15+00:00" data-title="May"><strong>May</strong> <em>05</em></time>
+            <h6 class="heading"><a href="#">Machine Sewing</a></h6>
+            <p>Tecah you to how to make use of sewing machine...&hellip;</p>
+          </div>
+        </article>
+      </li>
+      <li class="one_third">
+        <article class="element">
+          <figure><img src="images/demo/320x219.jpg" alt="">
+            <figcaption><a class="btn small" href="#">More</a></figcaption>
+          </figure>
+          <div class="excerpt">
+            <time datetime="2045-04-04T08:15+00:00" data-title="April"><strong>Apr</strong> <em>04</em></time>
+            <h6 class="heading"><a href="#">Hand embroidery</a></h6>
+            <p>Teach basic hand stitch & machine embriodery&hellip;</p>
+          </div>
+        </article>
+      </li>
+    </ul>
+    <p class="center nospace"><a class="btn" href="#">Ves</a></p>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay coloured" style="background-image:url('images/demo/backgrounds/02.jpg');">
+  <div class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <article class="center">
+      <h2 class="font-x3 uppercase">SewOll</h2>
+      <p class="btmspace-50 font-x2 capitalise">A complete sewing workshop</p>
+      <footer>
+        <ul class="nospace inline pushright">
+          <li><a class="btn inverse" href="#">Videos</a></li>
+          <li><a class="btn" href="#">Gallery</a></li>
+        </ul>
+      </footer>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="group">
+      <div class="one_third first btmspace-30">
+        <h6 class="font-x3 uppercase">START YOUR SEWING JOURNEY<a href="#">&raquo;</a></h6>
+      </div>
+      <article class="one_third btmspace-30">
+        <h6 class="uppercase font-x1"><a href="#">JO TICKLE</a></h6>
+        <p class="nospace">As a little girl Jo watched in awe as her Mum would work miracles with a piece of fabric, gradually turning it into pretty dresses for her and her sister. Even back then Jo knew that this was what she wanted to do.&hellip;</p>
+      </article>
+      <article class="one_third btmspace-30">
+        <h6 class="uppercase font-x1"><a href="#">NICOLA LEE</a></h6>
+        <p class="nospace">Making dolls clothes was Nicola’s first venture into sewing, fascinated and inspired by her Great Grandma Emily’s ability to turn her hand to all things stitched.&hellip;</p>
+      </article>
+      <article class="one_third first">
+        <h6 class="uppercase font-x1"><a href="#">Dapibus metus cras</a></h6>
+        <p class="nospace">Dictum morbi elementum accumsan nisi quis lacinia turpis tristique et phasellus a turpis eu libero mattis ultricies vel vitae nulla morbi ut rhoncus&hellip;</p>
+      </article>
+      <article class="one_third">
+        <h6 class="uppercase font-x1"><a href="#">Imperdiet nisl efficitur</a></h6>
+        <p class="nospace">Magna vivamus gravida lobortis arcu ut ullamcorper leo lacinia quis duis a massa eget quam efficitur sagittis morbi erat sapien imperdiet vitae augue&hellip;</p>
+      </article>
+      <article class="one_third">
+        <h6 class="uppercase font-x1"><a href="#">Gravida lorem ornare</a></h6>
+        <p class="nospace">Nec faucibus consectetur velit class aptent taciti sociosqu ad litora torquent per conubia nostra per inceptos himenaeos in auctor arcu vel dui gravida&hellip;</p>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4 bgded overlay" style="background-image:url('images/demo/backgrounds/01.jpg');">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_quarter first">
+      <h6 class="title">Libero semper nec</h6>
+      <p>Eros et magna ut vitae faucibus dui nec fermentum mauris nunc molestie venenatis metus et porttitor nisi faucibus ut cras non sem facilisis.</p>
+      <p>Hendrerit ligula ut blandit sem nulla non ipsum rutrum tempor dolor non faucibus eros cras imperdiet cursus sapien.</p>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Nisi ornare aliquet</h6>
+      <ul class="nospace linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          Street Name &amp; Number, Town, Postcode/Zip
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890<br>
+          +00 (123) 456 7890</li>
+        <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Donec at tellus eu</h6>
+      <ul class="nospace linklist">
+        <li><a href="#">Mauris in condimentum lacus</a></li>
+        <li><a href="#">Sed hendrerit est praesent</a></li>
+        <li><a href="#">Tempor dapibus enim phasellus</a></li>
+        <li><a href="#">Convallis lectus a tristique</a></li>
+        <li><a href="#">Lectus nisi sodales sem at</a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">ac urna morbi rutrum</h6>
+      <ul class="nospace linklist">
+        <li>
+          <article>
+            <h2 class="nospace font-x1"><a href="#">dignissim libero augue</a></h2>
+            <time class="font-xs block btmspace-10" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
+            <p class="nospace">aenean non egestas urna curabitur vitae aliquam felis&hellip;</p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <h2 class="nospace font-x1"><a href="#">et nisl id pretium</a></h2>
+            <time class="font-xs block btmspace-10" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
+            <p class="nospace">eros vehicula eros nec lacinia sem tellus vel turpis vivamus&hellip;</p>
+          </article>
+        </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">SewOll</a></p>
+    <p class="fl_right"> By <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">SewOll Group</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+</body>
+</html>
